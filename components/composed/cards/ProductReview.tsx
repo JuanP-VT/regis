3ce832047b1bd7b698dcc0340370e2d721f493ef
Juan Pablo Valdez <cv.juanp@gmail.com>
2024-02-09 19:25:47 -0700

    Refactor ProductReview component

--- components/composed/cards/ProductReview.tsx
+++ components/composed/cards/ProductReview.tsx
@@ -5,24 +5,7 @@ type Props = {};
 
 export default function ProductReview({}: Props) {
   return (
-    <div className="flex flex-col mt-5">
-      <div className="flex">
-        {Array(5)
-          .fill(0)
-          .map((_, index) => (
-            <StarIcon className="h-5 w-5 text-rose-400" key={index} />
-          ))}
-      </div>
-      <p className="py-1">
-        Grant program works wonderfully looks great,Grant program works
-        wonderfully looks great,Grant program works wonderfully looks great
-        Grant program works wonderfully looks greatGrant program works
-        wonderfully looks great
-      </p>
-      <div className="flex text-xs mt-1">
-        <p>Purchased Item:</p>
-        <p className="ml-1 text-blue-gray border-b">Сamping SVG Bundle...</p>
-      </div>
+    <div className="flex flex-col mt-5 border-b border-pink-100 p-3 rounded-lg">
       <div className="flex text-xs items-center mt-2">
         <Image
           src="https://picsum.photos/50/50"
@@ -34,9 +17,23 @@ export default function ProductReview({}: Props) {
         <p className="ml-3 border-b">John Doe Express</p>
         <p className="ml-3">August 10, 2024</p>
       </div>
-      <div className="flex text-xs font-bold mt-5 gap-2 items-center rounded-lg cursor-pointer hover:bg-slate-300 w-min p-2 ">
+      <div className="flex ml-12">
+        {Array(5)
+          .fill(0)
+          .map((_, index) => (
+            <StarIcon className="h-5 w-5 text-rose-400" key={index} />
+          ))}
+      </div>
+      <p className="py-1 text-sm">
+        Grant program works wonderfully looks great,Grant program works
+        wonderfully looks great,Grant program works wonderfully looks great
+        Grant program works wonderfully looks greatGrant program works
+        wonderfully looks great
+      </p>
+
+      <div className="flex text-xs font-bold  gap-2 items-center rounded-lg cursor-pointer hover:bg-slate-300 w-min p-2 ">
         <ThumbUp className="h-4 w-4 text-rose-400" />
-        Helpful?
+        Like
       </div>
     </div>
   );
