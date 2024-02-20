f577cb28b8d08640e6baa773645617b9124b8ba9
Juan Pablo Valdez <cv.juanp@gmail.com>
2024-02-20 09:13:48 -0700

    Add new store item page component

---
+++ app/admin/new-store-item/page.tsx
@@ -0,0 +1,12 @@
+import { Input } from "@/components/ui/input";
+import React from "react";
+
+type Props = {};
+
+export default function NewStoreItem({}: Props) {
+  return (
+    <div className="flex flex-col">
+      <Input />
+    </div>
+  );
+}
