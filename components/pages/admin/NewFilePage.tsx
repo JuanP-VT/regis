2cfb07b3fd6bc61f386075cd6f1f148260650aea
Juan Pablo Valdez <cv.juanp@gmail.com>
2024-03-06 11:22:40 -0700

    Refactor NewCutFilePage component

--- components/pages/admin/NewFilePage.tsx
+++ components/pages/admin/NewFilePage.tsx
@@ -8,6 +8,20 @@ import { z } from "zod";
 import LoadingButton from "@/components/LoadingButton";
 import AdminNav from "@/components/composed/AdminNav";
 
+/**
+ * This is the `NewCutFilePage` component.
+ *
+ * It's a form that allows users to upload a file, specifically with a `.zip` extension.
+ * The file is then sent to the `/api/new-file` endpoint for processing.
+ *
+ * The component handles both client-side and server-side validation:
+ * - Client-side validation is done using the `ValidateNewFileApi` schema validator.
+ * - Server-side validation feedback is displayed to the user.
+ *
+ * The component also provides feedback to the user in case of errors or successful file upload.
+ *
+ * The form includes a loading state, which is displayed as a `LoadingButton` while the file is being uploaded.
+ */
 export default function NewCutFilePage() {
   type Form = { file: File | null };
   const [formState, setFormState] = useState<Form>({
@@ -56,39 +70,44 @@ export default function NewCutFilePage() {
   return (
     <div>
       <AdminNav />
-      <div className="flex flex-col p-2">
-        <h1 className="p-2 text-2xl">Agregar Archivo</h1>
-        <p className="text-sm p-2">Solo archivos .zip</p>
-        <Input
-          className="cursor-pointer"
-          placeholder="Subir Archivo"
-          type="file"
-          onChange={(ev) => {
-            setErrorMessage([]);
-            setFeedback("");
-            if (ev.currentTarget.files) {
-              const file = ev.currentTarget.files[0];
-              setFormState((prevState) => ({
-                ...prevState,
-                file: file,
-              }));
-            }
-          }}
-        />
-        <div className="flex flex-col my-2 text-xs text-red-600 underline">
-          <p className="text-sm">{feedback}</p>
+      <div className="flex flex-col p-2 items-center">
+        <div>
+          <h1 className="p-2 text-2xl">Agregar Archivo Para Vender</h1>
+          <p className="text-sm p-2">
+            Solo se permiten archivos con la extensión <strong>.zip</strong>
+          </p>
+          <Input
+            className="cursor-pointer max-w-96"
+            lang="es"
+            placeholder="Subir Archivo"
+            type="file"
+            onChange={(ev) => {
+              setErrorMessage([]);
+              setFeedback("");
+              if (ev.currentTarget.files) {
+                const file = ev.currentTarget.files[0];
+                setFormState((prevState) => ({
+                  ...prevState,
+                  file: file,
+                }));
+              }
+            }}
+          />
+          <div className="flex flex-col my-2 text-xs text-red-600 underline">
+            <p className="text-sm">{feedback}</p>
 
-          {errorMessage.map((error, index) => (
-            <p key={index}>{error.message}</p>
-          ))}
+            {errorMessage.map((error, index) => (
+              <p key={index}>{error.message}</p>
+            ))}
+          </div>
+          {isLoading ? (
+            <LoadingButton isLoading={true} message="" />
+          ) : (
+            <Button className="w-20" onClick={handleSubmit}>
+              Agregar
+            </Button>
+          )}
         </div>
-        {isLoading ? (
-          <LoadingButton isLoading={true} message="" />
-        ) : (
-          <Button className="w-20" onClick={handleSubmit}>
-            Agregar
-          </Button>
-        )}
       </div>
     </div>
   );
