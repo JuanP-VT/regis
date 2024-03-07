cc404a7863e6325b65c63920f4996cc9eaca8ae0
Juan Pablo Valdez <cv.juanp@gmail.com>
2024-03-07 11:56:28 -0700

    Add Category API Endpoints

---
+++ app/api/categories/route.ts
@@ -0,0 +1,59 @@
+import { Category } from "@/types/category";
+import { getServerSession } from "next-auth";
+import { OPTIONS } from "../auth/[...nextauth]/nextAuthOptions";
+import { Role } from "@/types/user";
+import { NextResponse } from "next/server";
+import { validateNewStoreItem } from "@/lib/schema-validators/admin-new-storeitem";
+import { categoryModel } from "@/lib/models/category";
+
+export async function GET() {
+  try {
+    const collection = await categoryModel.find({});
+    return NextResponse.json(collection, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error con la base de datos" },
+      { status: 500 },
+    );
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    const session = await getServerSession(OPTIONS);
+    if (!session || session.user.role !== Role.ADMIN) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error al autentificar" },
+      { status: 500 },
+    );
+  }
+  const body = (await req.json()) as Category | undefined;
+  if (!body) {
+    return NextResponse.json({ message: "Invalid body" }, { status: 400 });
+  }
+
+  //Validate
+  const newCategory = {
+    name: body.name,
+    description: body.description,
+  };
+  try {
+    validateNewStoreItem.parse(newCategory);
+  } catch (error) {
+    return NextResponse.json({ message: "Failed Validation" }, { status: 400 });
+  }
+
+  //Save to DB
+  try {
+    await categoryModel.create(newCategory);
+    return NextResponse.json({ message: "Category saved" }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error saving category in the database" },
+      { status: 500 },
+    );
+  }
+}
