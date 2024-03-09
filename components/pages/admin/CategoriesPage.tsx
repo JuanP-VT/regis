6aa3945138b5f0ed541dc9fdb41d846117b8cf70
Juan Pablo Valdez <cv.juanp@gmail.com>
2024-03-09 11:09:23 -0700

    Add CreateCategories component for admin interface

---
+++ components/pages/admin/CategoriesPage.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { Category_ID } from "@/types/category";
+
+import {
+  Table,
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+} from "@/components/ui/table";
+
+import CategoryCard from "@/components/composed/cards/CategoryCard";
+import { CreateNewCategory } from "./CreateNewCategory";
+
+type Props = {
+  categoryList: Category_ID[];
+};
+
+export default function CategoriesPage({ categoryList }: Props) {
+  return (
+    <div className="">
+      <div className="">
+        <CreateNewCategory />
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="">Nombre</TableHead>
+              <TableHead className="">Descripción</TableHead>
+              <TableHead className="w-[100px]">Acciones</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {categoryList.map((category, index) => (
+              <CategoryCard category={category} key={`item${index}`} />
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  );
+}
