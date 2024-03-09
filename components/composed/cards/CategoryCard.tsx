1d90c64c4cc16f8992de6acc48897c5f2de90f14
Juan Pablo Valdez <cv.juanp@gmail.com>
2024-03-09 15:45:21 -0700

    Add CategoryCard component for category management in admin panel

---
+++ components/composed/cards/CategoryCard.tsx
@@ -0,0 +1,158 @@
+import { Input } from "@/components/ui/input";
+import { TableCell, TableRow } from "@/components/ui/table";
+import { Category_ID } from "@/types/category";
+import { CheckIcon, EditIcon, TrashIcon, Undo2Icon } from "lucide-react";
+import SyncLoader from "react-spinners/SyncLoader";
+import { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import PulseLoader from "react-spinners/PulseLoader";
+import { Button } from "@/components/ui/button";
+
+type Props = {
+  category: Category_ID;
+};
+
+// This component represents a category in the admin panel.
+// It has a default display mode and an edit mode for modifications.
+export default function CategoryCard({ category }: Props) {
+  const [isOnEditMode, setIsOnEditMode] = useState(false);
+  function DefaultDisplayMode({ category }: { category: Category_ID }) {
+    return (
+      <TableRow>
+        <TableCell className="font-medium">{category.name}</TableCell>
+        <TableCell className="font-medium">{category.description}</TableCell>
+        <TableCell className=" font-medium">
+          <div className="flex justify-center gap-2 px-2">
+            <EditIcon
+              className="h-4 w-4 cursor-pointer hover:scale-105 hover:text-pink-500"
+              onClick={() => setIsOnEditMode(true)}
+            />
+          </div>
+        </TableCell>
+      </TableRow>
+    );
+  }
+  function EditMode({ category }: { category: Category_ID }) {
+    const [formState, setFormState] = useState<Category_ID>({
+      _id: category._id,
+      name: category.name,
+      description: category.description,
+    });
+    const [isLoading, setIsLoading] = useState(false);
+    const [feedback, setFeedback] = useState("");
+    async function handleEdit() {
+      setIsLoading(true);
+      const reqBody: Category_ID = {
+        _id: formState._id,
+        name: formState.name,
+        description: formState.description,
+      };
+      await fetch("/api/categories/edit", {
+        method: "POST",
+        body: JSON.stringify(reqBody),
+      });
+
+      setIsLoading(false);
+      setIsOnEditMode(false);
+      location.reload();
+    }
+    async function handleDelete() {
+      setIsLoading(true);
+      setFeedback("");
+      const deleteResponse = await fetch("/api/categories/delete", {
+        method: "POST",
+        body: JSON.stringify({ _id: formState._id }),
+      });
+      console.log(deleteResponse.status);
+      if (deleteResponse.ok) {
+        setFeedback("Categoría eliminada");
+        location.reload();
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 2000);
+      }
+      if (deleteResponse.status === 402) {
+        setFeedback("La categoría está en uso");
+        setIsLoading(false);
+      }
+    }
+    return (
+      <TableRow>
+        <TableCell className="font-medium">
+          <Input
+            value={formState.name}
+            onChange={(ev) =>
+              setFormState((prev) => ({ ...prev, name: ev.target.value }))
+            }
+          />
+        </TableCell>
+        <TableCell className="font-medium">
+          <Input
+            value={formState.description}
+            onChange={(ev) =>
+              setFormState((prev) => ({
+                ...prev,
+                description: ev.target.value,
+              }))
+            }
+          />
+        </TableCell>
+        <TableCell className="font-medium">
+          <div className="flex justify-between px-2">
+            <Undo2Icon
+              className="h-4 w-4 cursor-pointer hover:scale-105 hover:text-pink-500"
+              onClick={() => setIsOnEditMode(false)}
+            />
+            {isLoading ? (
+              <SyncLoader size={2} />
+            ) : (
+              <CheckIcon
+                className="h-4 w-4 cursor-pointer hover:scale-105 hover:text-pink-500"
+                onClick={() => handleEdit()}
+              />
+            )}
+            <Dialog>
+              <DialogTrigger asChild>
+                <TrashIcon className="h-4 w-4 cursor-pointer hover:scale-105 hover:text-pink-500" />
+              </DialogTrigger>
+              <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                  <DialogTitle>¿Deseas Borrar Categoría?</DialogTitle>
+                  <DialogDescription>
+                    Los cambios son irreversibles, por seguridad, no podrás
+                    borrar categorías que estén en uso . <br />
+                  </DialogDescription>
+                </DialogHeader>
+                <div className="flex flex-col gap-4 py-4 text-sm">
+                  <p>Categoría: {category.name}</p>
+                </div>
+                <DialogFooter>
+                  {isLoading ? (
+                    <PulseLoader />
+                  ) : (
+                    <Button onClick={() => handleDelete()}>Borrar</Button>
+                  )}
+                  <p className="py-2 text-sm text-slate-600">{feedback}</p>
+                </DialogFooter>
+              </DialogContent>
+            </Dialog>
+          </div>
+        </TableCell>
+      </TableRow>
+    );
+  }
+
+  if (isOnEditMode) {
+    return <EditMode category={category} />;
+  }
+
+  return <DefaultDisplayMode category={category} />;
+}
