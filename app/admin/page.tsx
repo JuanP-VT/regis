9c3a911d24b85937541615824aa2cae1e26cf606
Juan Pablo Valdez <cv.juanp@gmail.com>
2024-03-09 11:08:00 -0700

    Added category fetching to admin page and passed category list to StoreItemsDisplay component

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -5,6 +5,7 @@ import { Role } from "@/types/user";
 import StoreItemsDisplay from "@/components/pages/admin/StoreItemsDisplay";
 import { StoreItemDB_ID } from "@/types/storeItemDB";
 import AdminNav from "@/components/composed/AdminNav";
+import { Category_ID } from "@/types/category";
 
 export default async function Admin() {
   try {
@@ -15,14 +16,25 @@ export default async function Admin() {
   } catch (error) {
     return <div>Error en la autentificación</div>;
   }
-  //Fetch store items from the database
-  const res = await fetch(`${process.env.URL}/api/store`);
-  const data = (await res.json()) as StoreItemDB_ID[];
 
-  return (
-    <div>
-      <AdminNav />
-      <StoreItemsDisplay storeItems={data} />
-    </div>
-  );
+  try {
+    //Fetch store items from the database
+    const res = await fetch(`${process.env.URL}/api/store`);
+    const data = (await res.json()) as StoreItemDB_ID[] | undefined;
+
+    const reqCategory = await fetch(`${process.env.URL}/api/categories`);
+    const categoryList = (await reqCategory.json()) as
+      | Category_ID[]
+      | undefined;
+
+    return (
+      <div>
+        <AdminNav />
+        <StoreItemsDisplay storeItems={data} categoryList={categoryList} />
+      </div>
+    );
+  } catch (error) {
+    console.error(error);
+    return <div>Error en el servidor</div>;
+  }
 }
