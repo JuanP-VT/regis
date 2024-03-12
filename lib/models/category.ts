e5ddca04033e82b8af672c2f4d777693ce30fbfa
Juan Pablo Valdez <cv.juanp@gmail.com>
2024-03-12 12:40:11 -0700

    Add subCategories field to Category model and update related validators and types

--- lib/models/category.ts
+++ lib/models/category.ts
@@ -2,15 +2,13 @@ import mongoose from "mongoose";
 
 // Define the schema for the Category model
 const categorySchema = new mongoose.Schema({
-  // The name of the category, which is required and must be unique
   name: { type: String, required: true, unique: true },
-  // The description of the category, which is also required
   description: { type: String, required: true },
+  subCategories: { type: [String], required: true },
 });
 
 // Create the Category model if it does not already exist, otherwise retrieve it
 const categoryModel =
   mongoose.models.Category || mongoose.model("Category", categorySchema);
 
-// Export the Category model for use in other files
 export { categoryModel };
--- lib/schema-validators/admin-new-category.ts
+++ lib/schema-validators/admin-new-category.ts
@@ -3,4 +3,5 @@ import { z } from "zod";
 export const validateNewCategory = z.object({
   name: z.string().min(2),
   description: z.string().min(1),
+  subCategories: z.array(z.string()),
 });
