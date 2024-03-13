61888b4cba497413c52f997b2f515e44356ea9fa
Juan Pablo Valdez <cv.juanp@gmail.com>
2024-03-13 11:58:20 -0700

    Refactor StoreItemDB interface and remove redundant newStoreItem.ts file

--- types/storeItemDB.ts
+++ types/storeItemDB.ts
@@ -1,5 +1,17 @@
-// Represents the structure of a store item  in the database
+// Represents the structure that a new store item should have
+export interface NewStoreItem {
+  fileName: string;
+  storeItemName: string;
+  details: string;
+  mainImageIndex: number;
+  images: File[] | null;
+  price: number;
+  discountPercentage: number;
+  categoryIDList: string[];
+  subCategoryIDList: string[];
+}
 
+// Represents the structure of a store item  in the database
 export interface StoreItemDB {
   fileName: string;
   storeItemName: string;
@@ -10,6 +22,7 @@ export interface StoreItemDB {
   discountPercentage: number;
   imageUrlList: string[];
   categoryIDList: string[];
+  subCategoryIDList: string[];
 }
 
 export interface StoreItemDB_ID extends StoreItemDB {
