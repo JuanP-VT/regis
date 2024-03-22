692e51d275847c9845e5836a60bf83d76e1e9df8
Juan Pablo Valdez <cv.juanp@gmail.com>
2024-03-22 14:25:31 -0700

    Add pagination component to CatalogPage

--- components/pages/CatalogPage.tsx
+++ components/pages/CatalogPage.tsx
@@ -2,11 +2,14 @@
 
 import { StoreItemDB_ID } from "@/types/storeItemDB";
 import CatalogItemCard from "../composed/cards/CatalogItemCard";
+import Pagination from "../composed/Pagination";
 
 type Props = {
   storeItems: StoreItemDB_ID[];
   category: string;
   subCategory: string;
+  categoryID: string;
+  subCategoryID: string;
   pagination: {
     totalItems: number;
     totalPages: number;
@@ -19,8 +22,18 @@ type Props = {
 export default function CatalogPage({
   storeItems,
   category,
+  categoryID,
+  subCategoryID,
   subCategory,
+  pagination,
 }: Props) {
+  if (storeItems.length === 0) {
+    return (
+      <div className="p-3 text-center italic underline">
+        No Se Encontraron Productos Con Estos Filtros
+      </div>
+    );
+  }
   return (
     <div className="flex flex-col justify-center ">
       <div className="flex justify-center border-b-2 border-b-slate-100 pb-7 text-3xl capitalize">
@@ -28,11 +41,19 @@ export default function CatalogPage({
         {subCategory && !category && <h1>Categoría - {subCategory}</h1>}
         {!category && !subCategory && <h1>Catálogo Completo</h1>}
       </div>
-      <div className="flex justify-center">
-        <div className="flex w-full max-w-[1100px] grid-cols-1 flex-col items-center justify-center gap-2 p-5 md:grid md:grid-cols-2 lg:grid-cols-3 ">
-          {storeItems?.map((item, index) => (
-            <CatalogItemCard key={index} storeItem={item} />
-          ))}
+      <div className="flex flex-col items-center">
+        <div className="max-w-[1100px]">
+          <div className="flex w-full  grid-cols-1 flex-col items-center justify-center gap-2 p-5 md:grid md:grid-cols-2 lg:grid-cols-3 ">
+            {storeItems?.map((item, index) => (
+              <CatalogItemCard key={index} storeItem={item} />
+            ))}
+          </div>
+          <Pagination
+            currentPage={pagination.currentPage}
+            numberOfPages={pagination.totalPages}
+            categoryID={categoryID}
+            subCategoryID={subCategoryID}
+          />
         </div>
       </div>
     </div>
