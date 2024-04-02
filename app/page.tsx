7ac6acc19ede3be88847a5f28341b621d0d9c45a
Juan Pablo Valdez <cv.juanp@gmail.com>
2024-04-02 15:47:34 -0700

    Refactor page layout and styles

--- app/page.tsx
+++ app/page.tsx
@@ -20,16 +20,16 @@ export default async function HomePage() {
             background="#f8fafc"
             className="absolute -z-10 h-full w-full "
           />
-          <div>
-            <Image
-              src="/banner.jpg"
-              alt="logo"
-              width={3200}
-              height={800}
-              className="z-10  self-center  rounded-lg object-contain    sm:h-64"
-            />
-          </div>
-          <Link href={"/catalog/category=&subCategory=&page=1"} className="">
+
+          <Image
+            src="/banner.jpg"
+            alt="logo"
+            width={3200}
+            height={800}
+            className="z-10  self-center  rounded-lg object-cover object-left pt-5 sm:h-64 sm:object-contain    sm:object-center"
+          />
+
+          <Link href={"/catalog/category=&subCategory=&page=1"} className="p-2">
             <Button
               size={"sm"}
               className="  bottom-0  z-10 transform  bg-rose-500 hover:bg-sky-400 md:px-16"
@@ -40,14 +40,14 @@ export default async function HomePage() {
         </div>
         <div className="relative "></div>
         <div className="mt-2 rounded-lg bg-zinc-200">
-          <section className="w-full">
+          <section className="group w-full">
             <div className="container px-4 md:px-6">
               <div className="grid items-center gap-6 lg:grid-cols-[1fr_600px] lg:gap-12 xl:grid-cols-[1fr_800px]">
                 <div className="space-y-2">
-                  <div className="inline-block rounded-lg bg-gray-100 px-3 py-1 text-sm dark:bg-gray-800">
+                  <div className="mt-2 inline-block rounded-lg bg-gray-100 px-3 py-1 text-sm transition-all duration-1000 group-hover:bg-sky-200 dark:bg-gray-800">
                     Archivos Digitales
                   </div>
-                  <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                  <h1 className="pt-2 text-3xl font-bold tracking-tighter sm:text-5xl">
                     Los Mejores Diseños
                   </h1>
                   <p className="max-w-[600px] text-gray-500 dark:text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
@@ -58,7 +58,7 @@ export default async function HomePage() {
                 </div>
                 <Image
                   alt="Banner"
-                  className="aspect-video overflow-hidden rounded-full object-contain object-center p-3"
+                  className="aspect-video overflow-hidden rounded-full object-contain object-center p-3 "
                   height="450"
                   src="/logo.jfif"
                   width="600"
