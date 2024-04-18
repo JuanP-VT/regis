f98b7c480f97ddf20dddadcee324f92c239c181f
Juan Pablo Valdez <cv.juanp@gmail.com>
2024-04-18 12:10:08 -0700

    Add "light-brown" color to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,7 @@ const config = {
     },
     extend: {
       colors: {
+        "light-brown": "#eaded6",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
