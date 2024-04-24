a31bbcc405b4cc0842e2dcecd3387dfdff9fc731
Juan Pablo Valdez <cv.juanp@gmail.com>
2024-04-24 08:30:21 -0700

    Update TopNav component with new links and icons

--- components/composed/TopNav.tsx
+++ components/composed/TopNav.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { People } from "@mui/icons-material";
 import { Button } from "@/components/ui/button";
-
+import CartIcon from "@/public/icon/cart.svg";
 import Link from "next/link";
 import SignInButton from "./SignInButton";
 import { Session } from "next-auth";
 import ShoppingCartIcon from "./ShoppingCartIcon";
 import { DatabaseIcon } from "lucide-react";
+import Image from "next/image";
 
 /* 
   Navigation bar used in the store
@@ -16,34 +17,60 @@ type Props = {
 };
 export default function TopNav({ session }: Props) {
   return (
-    <header className="flex h-12 items-center border-b bg-slate-100 px-1 md:px-6 lg:h-14 ">
+    <header className="flex h-12 items-center border-b bg-melon px-1 md:px-6 lg:h-14 ">
       <div className="flex gap-2 px-1 text-sm md:gap-8">
-        <Link className="text-xs font-semibold sm:text-base" href="/">
+        <Link className="text-xs font-semibold sm:text-sm" href="/">
           Inicio
         </Link>
         <Link
-          className="text-xs font-semibold sm:text-base"
+          className="text-xs font-semibold sm:text-sm"
           href="/catalog/category=&subCategory=&page=1"
         >
           Catálogo
+        </Link>
+        <Link
+          className="hidden text-xs font-semibold sm:inline sm:text-sm"
+          href="/freebies"
+        >
+          Freebies
+        </Link>
+        <Link
+          className="hidden text-xs font-semibold sm:inline sm:text-sm"
+          href="/sales"
+        >
+          Rebajas
+        </Link>
+        <Link
+          className="hidden text-xs font-semibold sm:inline sm:text-sm"
+          href="/contact"
+        >
+          Contacto
+        </Link>
+        <Link
+          className="hidden text-xs font-semibold sm:inline sm:text-sm"
+          href="/nosotros"
+        >
+          Nosotros
         </Link>
       </div>
 
       <div className="ml-auto flex items-center gap-1  lg:gap-8">
         {session && session.user.role === "admin" && (
           <Link href="/admin">
-            <Button size="icon" variant="outline">
-              <DatabaseIcon className="h-4 w-4" />
-              <span className="sr-only">Admin</span>
-            </Button>
+            <DatabaseIcon className="mt-1 h-4 w-4 scale-150 text-rose-400" />
+            <span className="sr-only">Admin</span>
           </Link>
         )}
         <ShoppingCartIcon />
-        <Link href="/me">
-          <Button size="icon" variant="outline">
-            <People className="h-4 w-4" />
-            <span className="sr-only">Perfil</span>
-          </Button>
+        <Link href="/me" className="relative flex h-8 w-8">
+          <Image
+            alt="Perfil"
+            width={30}
+            height={30}
+            src="icon/user.svg"
+            className="pointer-events-none h-full w-full  object-contain"
+          />
+          <span className="sr-only">Perfil</span>
         </Link>
         <SignInButton session={session} />
       </div>
