902c9dc99ea3feea6fff56deb02bb0f89e63ffe6
Juan Pablo Valdez <cv.juanp@gmail.com>
2024-04-24 12:15:00 -0700

    Update next.config.mjs to include remotePatterns for image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "picsum.photos",
-      "d3brki6ihgj5xt.cloudfront.net",
-      "lh3.googleusercontent.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "d3brki6ihgj5xt.cloudfront.net",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "**",
+      },
     ],
   },
 };
 
 export default nextConfig;
+
+/**
+ *     domains: ["d3brki6ihgj5xt.cloudfront.net", "lh3.googleusercontent.com"],
+
+ *  images: {
+    
+  },
+ */
