acd7a7fbaa549b16efea2cd64006e85bd855b9b7
Juan Pablo Valdez <cv.juanp@gmail.com>
2024-04-25 08:45:19 -0700

    Update styles and links in AdminNav component

--- components/composed/AdminNav.tsx
+++ components/composed/AdminNav.tsx
@@ -7,32 +7,26 @@ import Link from "next/link";
 export default function AdminNav() {
   return (
     <div className="flex w-full gap-4 rounded-lg border p-3 text-sm font-semibold  ">
-      <Link
-        className="cursor-pointer hover:scale-105 hover:text-pink-500"
-        href="/admin/files"
-      >
+      <Link className="cursor-pointer  hover:text-sky-500" href="/admin/files">
         Mis Archivos
       </Link>
       <Link
-        className="cursor-pointer hover:scale-105 hover:text-pink-500"
+        className="cursor-pointer  hover:text-sky-500"
         href="/admin/new-file"
       >
         Agregar Archivo
       </Link>
-      <Link
-        className="cursor-pointer hover:scale-105 hover:text-pink-500"
-        href="/admin"
-      >
+      <Link className="cursor-pointer  hover:text-sky-500" href="/admin">
         Registros En La Tienda
       </Link>
       <Link
-        className="cursor-pointer hover:scale-105 hover:text-pink-500"
+        className="cursor-pointer  hover:text-sky-500"
         href="/admin/new-store-item"
       >
         Agregar Registro A Tienda
       </Link>
       <Link
-        className="cursor-pointer hover:scale-105 hover:text-pink-500"
+        className="cursor-pointer  hover:text-sky-500"
         href="/admin/categories"
       >
         Categorías
