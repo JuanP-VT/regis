3dca0a7ad6f12b39d4c97c33df02f6407ebff355
Juan Pablo Valdez <cv.juanp@gmail.com>
2024-04-29 09:52:13 -0700

    Update FreebiesPage component to fetch and display free items

--- app/freebies/page.tsx
+++ app/freebies/page.tsx
@@ -1,3 +1,17 @@
-export default function FreebiesPage() {
-  return <div>En Construcción...</div>;
+import dbConnect from "@/lib/dbConnect";
+import { getServerSession } from "next-auth";
+import { OPTIONS } from "../api/auth/[...nextauth]/nextAuthOptions";
+import { StoreItemModel } from "@/lib/models/storeItem";
+import FreebiesPage from "@/components/pages/FreebiesPage";
+import { StoreItemDB_ID } from "@/types/storeItemDB";
+
+export default async function Freebies() {
+  const session = await getServerSession(OPTIONS);
+  await dbConnect();
+  //Freebies are going to be items that are either free or have a 100% discount
+  const freebies = await StoreItemModel.find({
+    $or: [{ price: 0 }, { discountPercentage: 100 }],
+  });
+  const freebieList = JSON.parse(JSON.stringify(freebies)) as StoreItemDB_ID[]; //parse and stringify to remove mongoose methods
+  return <FreebiesPage freebies={freebieList} session={session} />;
 }
