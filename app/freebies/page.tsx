b56164a88bdd62964f34792206e42ca831ad771f
Juan Pablo Valdez <cv.juanp@gmail.com>
2024-04-29 12:20:19 -0700

    Refactor FreebiesPage component to include error handling

--- app/freebies/page.tsx
+++ app/freebies/page.tsx
@@ -8,20 +8,27 @@ import { Category_ID } from "@/types/category";
 import { categoryModel } from "@/lib/models/category";
 
 export default async function Freebies() {
-  const session = await getServerSession(OPTIONS);
-  await dbConnect();
-  //Freebies are going to be items that are either free or have a 100% discount
-  const freebies = await StoreItemModel.find({
-    $or: [{ price: 0 }, { discountPercentage: 100 }],
-  });
-  const freebieList = JSON.parse(JSON.stringify(freebies)) as StoreItemDB_ID[]; //parse and stringify to remove mongoose methods
-  const categoryData = (await categoryModel.find({}).lean()) as Category_ID[];
-  const categoryList = JSON.parse(JSON.stringify(categoryData));
-  return (
-    <FreebiesPage
-      freebies={freebieList}
-      session={session}
-      categoryList={categoryList}
-    />
-  );
+  try {
+    const session = await getServerSession(OPTIONS);
+    await dbConnect();
+    //Freebies are items that are free; price is 0 or discount is 100%
+    const freebies = await StoreItemModel.find({
+      $or: [{ price: 0 }, { discountPercentage: 100 }],
+    });
+    const freebieList = JSON.parse(
+      JSON.stringify(freebies),
+    ) as StoreItemDB_ID[]; //parse and stringify to remove mongoose methods
+    const categoryData = (await categoryModel.find({}).lean()) as Category_ID[];
+    const categoryList = JSON.parse(JSON.stringify(categoryData));
+    return (
+      <FreebiesPage
+        freebies={freebieList}
+        session={session}
+        categoryList={categoryList}
+      />
+    );
+  } catch (error) {
+    console.error(error);
+    return <div>Algo salió mal</div>;
+  }
 }
