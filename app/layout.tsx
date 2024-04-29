2eda0e6e422da1430dd14fc2d836fdba2ed1d654
Juan Pablo Valdez <cv.juanp@gmail.com>
2024-04-29 12:33:53 -0700

    Add dynamic export to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import TopNav from "@/components/composed/TopNav";
 import { Toaster } from "sonner";
 import { getServerSession } from "next-auth";
 import { OPTIONS } from "./api/auth/[...nextauth]/nextAuthOptions";
-
+export const dynamic = "force-dynamic";
 const raleway = Raleway({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Regina & Gael Studio Beta",
